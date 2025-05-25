cfd99d9c404ad8a7670327e778d603d74b833104
Sagar Deep Saha <sagarsaha@outlook.in>
2025-05-25 19:36:42 +0530

    modified api.js in services

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://ats-back-end.vercel.app/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://ats-backend-teal.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
